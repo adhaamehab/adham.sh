3a14a38ed96cc0491eef3f36298a87bd675ca3a1
Adham <adhaamehab.me@gmail.com>
2023-05-25 18:49:11 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,37 +9,19 @@ export default function Home() {
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
         <section class="text-gray-600 body-font">
           <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-            <img
-              class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded-full"
-              alt="us"
-              src="/avatar.JPG"
-            ></img>
             <h1 class="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
               Hi, I'm Adham 👋
             </h1>
             <h2 class="font-bold text-xl md:text-xl tracking-tight mb-4 text-black dark:text-white">
-              I'm a Software Engineer from Cairo, Egypt 🇪🇬
+             <div>
+              I am a Senior Staff Software Engineer at <a href="https://cision.com"> Cision </a>.
+              <div>
             </h2>
             <h2 class="text-left text-black dark:text-white">
               <div>
-                I'm currently working at <a href="https://factmata.com"> Factmata</a> as a Staff Site Reliability Engineer. Where we are
-                working on fighting misinformation and fake news.
-              </div>
+                Before that, I was the Head of Engineering at <a href="https://factmata.com"> Factmata</a>, Where we got aquired by Cision.
               <div>
-                Before that I was an early engineer at{" "}
-                <a href="https://arqamfc.com"> Arqam </a> where we merged with
-                <a href="https://statsbomb.com"> Statsbomb </a> and the company
-                now is the leading sports data company in the world.
-              </div>
-              <div>
-                For the past 5 years I've been working for startups. Focusing on
-                the intersection between Product Development, Data and ML
-                Infrastructure.
-              </div>
-              <div>
-                I've a Bachelor of Computer Science from Ainshams University.
-                When I'm not working I try to mentor Students and other
-                Engineers to get into more advanced CS and engineering topics
+                I also worked at <a href="https://arqamfc.com"> Arqam </a> where we got acquired by <a href="https://statsbomb.com"> Statsbomb </a>. I 
               </div>
             </h2>
           </div>
